b15578bc7c941483d8317ed9ae609ce18597a90f
Domeniko Gentner <source@tuxstash.de>
2020-11-07 16:30:15 +0100

    removed PIL

--- painter/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# /**********************************************************************************
-#  * _author  : Domeniko Gentner
-#  * _mail    : code@tuxstash.de
-#  * _repo    : https://git.tuxstash.de/gothseidank/tweet2png
-#  * _license : This project is under MIT License
-#  *********************************************************************************/
-from .painter import painter
